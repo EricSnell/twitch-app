f4fdf05f02c23bba5c937c17cf69e6cba306f002
Eric Snell <eric_snell21@yahoo.com>
2018-01-20 16:15:18 -0500

    project outlined

---
+++ app.js
@@ -0,0 +1,24 @@
+/*
+  User stories:
+    
+    -- Show list of users who are currently streaming
+    -- Show list of users who are currently offline
+    -- Show list of all users
+
+    -- If user is streaming, display the stream title
+    -- Upon clicking a user who is currently streaming, it will direct to the stream
+    
+  *BONUS*
+
+    -- Live search field
+
+
+  API 
+
+    -- Base URL:  https://wind-bow.gomix.me/twitch-api
+    -- routes (GET only):
+                - /users/:user
+                - /channels/:channel
+                - /streams/:stream
+
+*/
